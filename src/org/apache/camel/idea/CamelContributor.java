25a7716fe24d5c66148cf8c15cd9534be045fd92
fharms <flemming.harms@gmail.com>
2016-12-29 22:48:55 +0100

    #24 Add support for smart completion for property placeholders
    The current implementation is scanning all property files inside
    the project. This mean it has no knowledge about if the spring
     context actually is importing the property file.

--- src/org/apache/camel/idea/CamelContributor.java
+++ src/org/apache/camel/idea/CamelContributor.java
@@ -5,9 +5,9 @@
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,21 +16,29 @@
  */
 package org.apache.camel.idea;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
 import com.intellij.codeInsight.completion.CompletionContributor;
 import com.intellij.codeInsight.completion.CompletionParameters;
 import com.intellij.codeInsight.completion.CompletionProvider;
 import com.intellij.codeInsight.completion.CompletionResultSet;
 import com.intellij.codeInsight.completion.CompletionUtil;
+import com.intellij.codeInsight.completion.PlainPrefixMatcher;
 import com.intellij.codeInsight.lookup.LookupElement;
+import com.intellij.codeInsight.lookup.LookupElementBuilder;
+import com.intellij.openapi.vfs.VfsUtil;
 import com.intellij.util.ProcessingContext;
 import org.apache.camel.catalog.CamelCatalog;
 import org.apache.camel.catalog.DefaultCamelCatalog;
 import org.apache.camel.catalog.JSonSchemaHelper;
 import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
 
 import static org.apache.camel.idea.CamelSmartCompletionEndpointOptions.addSmartCompletionSuggestions;
 import static org.apache.camel.idea.CamelSmartCompletionEndpointValue.addSmartCompletionForSingleValue;
@@ -42,6 +50,8 @@ import static org.apache.camel.idea.CamelSmartCompletionEndpointValue.addSmartCo
 public class CamelContributor extends CompletionContributor {
 
     private static final CamelCatalog camelCatalog = new DefaultCamelCatalog(true);
+    //TODO Allow this to be configurable
+    private static final List<String> IGNORE_PROPERTIES = Arrays.asList("java.", "Logger.", "logger", "appender.", "rootLogger.");
 
     /**
      * Smart completion for Camel endpoints.
@@ -49,23 +59,14 @@ public class CamelContributor extends CompletionContributor {
     static protected class EndpointCompletion extends CompletionProvider<CompletionParameters> {
 
         public void addCompletions(@NotNull CompletionParameters parameters,
-                ProcessingContext context,
-                @NotNull CompletionResultSet resultSet) {
+                                   ProcessingContext context,
+                                   @NotNull CompletionResultSet resultSet) {
             // is this a possible Camel endpoint uri which we know
-            String val = parameters.getPosition().getText();
-            int hackIndex = val.indexOf(CompletionUtil.DUMMY_IDENTIFIER);
-            if (hackIndex == -1) {
-                hackIndex = val.indexOf(CompletionUtil.DUMMY_IDENTIFIER_TRIMMED);
-            }
-            if (hackIndex > -1) {
-                val = val.substring(0, hackIndex);
-            }
+            String val = parsePsiElement(parameters);
 
-            if (val.startsWith("\"")) {
-                val = val.substring(1);
-            }
-            if (val.endsWith("\"")) {
-                val = val.substring(0,val.length()-1);
+            if (val.endsWith("{{")) {
+                propertyPlaceholdersSmartCompletion(parameters, resultSet);
+                return; //we are done
             }
 
             String componentName = StringUtils.asComponentName(val);
@@ -109,6 +110,57 @@ public class CamelContributor extends CompletionContributor {
                 }
             }
         }
+    }
+
+    /**
+     * Parse the PSI text {@link CompletionUtil#DUMMY_IDENTIFIER} and " character and remove them
+     * @param parameters - completion parameter to parse
+     * @return new string stripped for any {@link CompletionUtil#DUMMY_IDENTIFIER} and " character
+     */
+    @NotNull
+    private static String parsePsiElement(@NotNull CompletionParameters parameters) {
+        String val = parameters.getPosition().getText();
+        int hackIndex = val.indexOf(CompletionUtil.DUMMY_IDENTIFIER);
+        if (hackIndex == -1) {
+            hackIndex = val.indexOf(CompletionUtil.DUMMY_IDENTIFIER_TRIMMED);
+        }
+        if (hackIndex > -1) {
+            val = val.substring(0, hackIndex);
+        }
+
+        if (val.startsWith("\"")) {
+            val = val.substring(1);
+        }
+        if (val.endsWith("\"")) {
+            val = val.substring(0, val.length() - 1);
+        }
+        return val;
+    }
+
+    private static void propertyPlaceholdersSmartCompletion(@NotNull CompletionParameters parameters, @NotNull CompletionResultSet resultSet) {
+        VfsUtil.processFilesRecursively(parameters.getOriginalFile().getManager().getProject().getBaseDir(), virtualFile -> {
+            if (virtualFile.getName().endsWith(".properties")) {
+                File file = new File(virtualFile.getPath());
+                Properties properties = new Properties();
+                try {
+                    properties.load(Files.newInputStream(file.toPath()));
+                } catch (IOException e) {
+                }//TODO : log a warning, but for now we ignore it and continue.
+
+                properties.forEach((key, value) -> {
+                            String keyStr = (String) key;
+                            boolean noneMatch = IGNORE_PROPERTIES.stream().noneMatch(s -> keyStr.startsWith(s));
+                            if (noneMatch) {
+                                LookupElementBuilder builder = LookupElementBuilder.create(keyStr+"}}")
+                                        .appendTailText((String) value, true)
+                                        .withPresentableText(keyStr + " = ");
+                                resultSet.withPrefixMatcher(new PlainPrefixMatcher("")).addElement(builder);
+                            }
+                        }
+                );
+            }
+            return true;
+        });
     }
 
 }
