65930de36de45e6b83c3be7c91fc7d33b567725e
adrianbumbas <a.bumbas@gmail.com>
2017-01-05 09:48:12 +0100

    #35 Fixed Checkstyle warnings

--- src/main/java/org/apache/camel/idea/completion/extension/CamelRouteLineMarkerProvider.java
+++ src/main/java/org/apache/camel/idea/completion/extension/CamelRouteLineMarkerProvider.java
@@ -5,9 +5,9 @@
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,6 +16,7 @@
  */
 package org.apache.camel.idea.completion.extension;
 
+import java.util.Collection;
 import com.intellij.codeInsight.daemon.RelatedItemLineMarkerInfo;
 import com.intellij.codeInsight.daemon.RelatedItemLineMarkerProvider;
 import com.intellij.codeInsight.navigation.NavigationGutterIconBuilder;
@@ -27,8 +28,6 @@ import com.intellij.psi.PsiLiteralExpression;
 import org.apache.camel.idea.catalog.CamelCatalogService;
 import org.apache.camel.idea.util.StringUtils;
 import org.jetbrains.annotations.NotNull;
-
-import java.util.Collection;
 
 /**
  * Provider that adds the Camel icon in the gutter when it detects a Camel route.
