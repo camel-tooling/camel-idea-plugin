0bf758cd531bb224441e4d91265cdc581d5ec709
Bennet Schulz <mail@bennet-schulz.de>
2017-10-07 12:33:02 +0200

    fix import order to avoid checkstyle errors

--- camel-idea-plugin/src/test/java/org/apache/camel/idea/util/StringUtilsTest.java
+++ camel-idea-plugin/src/test/java/org/apache/camel/idea/util/StringUtilsTest.java
@@ -23,7 +23,10 @@ import java.util.Map;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 public class StringUtilsTest {
 
