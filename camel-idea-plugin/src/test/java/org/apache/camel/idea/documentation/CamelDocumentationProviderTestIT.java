a8b0685319c30f093b31e0c26b25ba8402b1998f
Bennet Schulz <mail@bennet-schulz.de>
2017-10-09 13:58:16 +0200

    add test for generateDoc() method

--- camel-idea-plugin/src/test/java/org/apache/camel/idea/documentation/CamelDocumentationProviderTestIT.java
+++ camel-idea-plugin/src/test/java/org/apache/camel/idea/documentation/CamelDocumentationProviderTestIT.java
@@ -16,14 +16,16 @@
  */
 package org.apache.camel.idea.documentation;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 
 import com.intellij.ide.highlighter.JavaFileType;
 import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiJavaFile;
+import com.intellij.psi.PsiLiteralExpression;
+import com.intellij.psi.PsiManager;
 import com.intellij.psi.PsiReference;
 import org.apache.camel.idea.CamelLightCodeInsightFixtureTestCaseIT;
 
@@ -32,14 +34,25 @@ import org.apache.camel.idea.CamelLightCodeInsightFixtureTestCaseIT;
  */
 public class CamelDocumentationProviderTestIT extends CamelLightCodeInsightFixtureTestCaseIT {
 
-    public String getJavaTestData() {
+    private String getJavaTestData() {
         return "public static class MyRouteBuilder extends RouteBuilder {\n"
-            + "        @Override\n"
-            + "        public void configure() throws Exception {\n"
-            + "            <caret>from(\"file:inbox?delete=true\")\n"
-            + "                .to(\"file:outbox\");\n"
-            + "        }\n"
-            + "    }";
+                + "        @Override\n"
+                + "        public void configure() throws Exception {\n"
+                + "            <caret>from(\"file:inbox?delete=true\")\n"
+                + "                .to(\"file:outbox\");\n"
+                + "        }\n"
+                + "    }";
+
+    }
+
+    private String getJavaTestData1() {
+        return "public static class MyRouteBuilder extends RouteBuilder {\n"
+                + "        @Override\n"
+                + "        public void configure() throws Exception {\n"
+                + "            from(\"file:inbox?<caret>\")\n"
+                + "                .to(\"file:outbox\");\n"
+                + "        }\n"
+                + "    }";
 
     }
 
@@ -57,7 +70,31 @@ public class CamelDocumentationProviderTestIT extends CamelLightCodeInsightFixtu
 
         String docInfo = new CamelDocumentationProvider().getQuickNavigateInfo(psiClass, referenceElement.getElement());
         assertNotNull(docInfo);
+        String s = exampleHtmlFileText(getTestName(true));
         assertEquals(exampleHtmlFileText(getTestName(true)), docInfo);
+    }
+
+    public void testGetUrlFor() {
+        assertNull(new CamelDocumentationProvider().getUrlFor(null, null));
+    }
+
+    public void testGenerateDoc() throws Exception {
+        myFixture.configureByText(JavaFileType.INSTANCE, getJavaTestData1());
+
+        PsiElement element = myFixture.findElementByText("\"file:inbox?\"", PsiLiteralExpression.class);
+        String componentName = "file";
+        String lookup = componentName + ":inbox?delete";
+        PsiManager manager = myFixture.getPsiManager();
+
+        PsiElement docInfo = new CamelDocumentationProvider().getDocumentationElementForLookupItem(manager, lookup, element);
+
+        String doc = new CamelDocumentationProvider().generateDoc(docInfo, null);
+        assertEquals(readExpectedFile(), doc);
+
+        String documentation = new CamelDocumentationProvider().generateDoc(element, null);
+        assertNotNull(documentation);
+        assertTrue(documentation.startsWith("<b>File Component</b><br/>The file component is used for reading or writing files.<br/>"));
+        System.out.println(documentation);
     }
 
     private PsiClass getTestClass() throws Exception {
@@ -69,4 +106,24 @@ public class CamelDocumentationProviderTestIT extends CamelLightCodeInsightFixtu
         return StringUtil.convertLineSeparators(FileUtil.loadFile(htmlPath).trim(), "");
     }
 
+    private String readExpectedFile() {
+        String fileContent = null;
+        try {
+            fileContent = getFileContent(new FileInputStream("src/test/resources/testData/expected_doc.xml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return fileContent;
+    }
+
+    private static String getFileContent(FileInputStream fis) throws IOException {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(fis))) {
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+            return sb.toString();
+        }
+    }
 }
