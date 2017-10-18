133f4169c5042ebf815cbb49385f9467c23b9e2b
Bennet Schulz <mail@bennet-schulz.de>
2017-10-18 12:20:08 +0200

    move classes to sub package

--- camel-idea-plugin/src/main/java/org/apache/camel/idea/preference/properties/CamelEditDialog.java
+++ camel-idea-plugin/src/main/java/org/apache/camel/idea/preference/properties/CamelEditDialog.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.camel.idea.preference;
+package org.apache.camel.idea.preference.properties;
 
 import java.awt.*;
 import javax.swing.*;
