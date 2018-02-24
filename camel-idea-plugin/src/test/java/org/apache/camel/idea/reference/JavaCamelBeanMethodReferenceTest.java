2df5b990a1b6bd135ad2fa50b4974643a5aad097
Flemming Harms <flemming.harms@gmail.com>
2018-02-24 16:08:52 +0100

    Disable soft reference for bean methods reference.
    This solve the issue with method reference in Camel DSL is also
    linked to any matching property names. So when jump to declaration
    it would also present choices found in properties files

---
+++ camel-idea-plugin/src/test/java/org/apache/camel/idea/reference/JavaCamelBeanMethodReferenceTest.java
@@ -0,0 +1,52 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.idea.reference;
+
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.ResolveResult;
+import org.apache.camel.idea.CamelLightCodeInsightFixtureTestCaseIT;
+import org.apache.camel.idea.refereance.CamelBeanMethodReference;
+
+/**
+ * Test method reference link between the Java Camel DSL bean reference method @{code bean(MyClass.class,"myMethodName")}
+ */
+public class JavaCamelBeanMethodReferenceTest extends CamelLightCodeInsightFixtureTestCaseIT {
+
+    public void testCamelMethodReference() {
+        myFixture.configureByFiles("CompleteJavaBeanRoute3TestData.java", "CompleteJavaBeanTestData.java",
+            "CompleteJavaBeanSuperClassTestData.java", "CompleteJavaBeanMethodPropertyTestData.properties");
+        PsiElement element = myFixture.getFile().findElementAt(myFixture.getCaretOffset()).getParent();
+        final ResolveResult[] resolveResults = ((CamelBeanMethodReference) element.getReferences()[0]).multiResolve(false);
+        assertEquals(1, resolveResults.length);
+        assertEquals("public void letsDoThis() {}",  resolveResults[0].getElement().getText());
+    }
+
+    public void testCamelMultipleMethodReference() {
+        myFixture.configureByFiles("CompleteJavaBeanRoute4TestData.java", "CompleteJavaBeanMultipleMethodTestData.java", "CompleteJavaBeanSuperClassTestData.java");
+        PsiElement element = myFixture.getFile().findElementAt(myFixture.getCaretOffset()).getParent();
+        final ResolveResult[] resolveResults = ((CamelBeanMethodReference) element.getReferences()[0]).multiResolve(false);
+        assertEquals(2, resolveResults.length);
+        assertEquals("public void multipleMethodsWithSameName() { }",  resolveResults[0].getElement().getText());
+        assertEquals("public void multipleMethodsWithSameName(int data) { }",  resolveResults[1].getElement().getText());
+    }
+
+    public void testCamelNoMethodReference() {
+        myFixture.configureByFiles("CompleteJavaBeanRoute4TestData.java");
+        PsiElement element = myFixture.getFile().findElementAt(myFixture.getCaretOffset()).getParent();
+        assertEquals(0, element.getReferences().length);
+    }
+}
