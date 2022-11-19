bdb64370deb70e337330454ae57ab3efd6c4998b
Nicolas Filotto <essobedo@users.noreply.github.com>
2022-11-19 10:44:55 +0100

    Ref #769: Allow to load a runtime provider w/o the core artifacts (#771)
    ## Motivation
    
    When we force a runtime by settings if the corresponding core artifacts are not part of the dependencies of the project, the catalog is not downloaded while we would expect to download it anyway.
    
    ## Modifications:
    
    * Add a fallback method that will retrieve the catalog corresponding to the version of Camel
    * Do nothing in the case of Quarkus as the versions are different
    * Add a specific `grapeConfig.xml` as a temporary workaround to prevent dependency retrieval issue

---
+++ camel-idea-plugin/src/test/java/com/github/cameltooling/idea/service/CamelCatalogProviderWithSpringBootArtifactTestIT.java
@@ -0,0 +1,58 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.cameltooling.idea.service;
+
+import com.github.cameltooling.idea.CamelLightCodeInsightFixtureTestCaseIT;
+import com.github.cameltooling.idea.catalog.CamelCatalogProvider;
+import org.apache.camel.catalog.CamelCatalog;
+import org.apache.camel.catalog.DefaultCamelCatalog;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * The integration tests ensuring that the {@link CamelCatalogProvider} works as expected in case the core artifact of
+ * Camel Spring Boot is part of the dependencies of the project.
+ */
+public class CamelCatalogProviderWithSpringBootArtifactTestIT extends CamelLightCodeInsightFixtureTestCaseIT {
+
+    private static final String CORE_RUNTIME_MAVEN_ARTIFACT = "org.apache.camel.springboot:camel-spring-boot:3.18.1";
+
+    private final CamelCatalogProvider provider = CamelCatalogProvider.SPRING_BOOT;
+
+    @Nullable
+    @Override
+    protected String[] getMavenDependencies() {
+        return new String[]{CORE_RUNTIME_MAVEN_ARTIFACT};
+    }
+
+    /**
+     * Ensure that the Catalog can be retrieved.
+     */
+    public void testGetCatalog() {
+        CamelCatalog catalog = provider.get(getProject());
+        assertNotNull(catalog.getRuntimeProvider());
+        assertSame(catalog, catalog.getRuntimeProvider().getCamelCatalog());
+        assertInstanceOf(catalog, DefaultCamelCatalog.class);
+    }
+
+    /**
+     * Ensure that the runtime provider can be loaded.
+     */
+    public void testRuntimeProvider() {
+        getProject().getService(CamelCatalogService.class).get().setVersionManager(new CamelMavenVersionManager());
+        assertTrue(provider.loadRuntimeProviderVersion(getProject()));
+    }
+}
---
+++ camel-idea-plugin/src/test/java/com/github/cameltooling/idea/service/CamelCatalogProviderWithoutRuntimeArtifactTestIT.java
@@ -0,0 +1,82 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.cameltooling.idea.service;
+
+import com.github.cameltooling.idea.CamelLightCodeInsightFixtureTestCaseIT;
+import com.github.cameltooling.idea.catalog.CamelCatalogProvider;
+import com.github.cameltooling.idea.util.ArtifactCoordinates;
+import com.intellij.testFramework.ServiceContainerUtil;
+import org.apache.camel.catalog.CamelCatalog;
+import org.apache.camel.catalog.DefaultCamelCatalog;
+
+/**
+ * The integration tests ensuring that the {@link CamelCatalogProvider} works as expected in case the core artifact of
+ * a Camel Runtime is not part of the dependencies of the project.
+ */
+public class CamelCatalogProviderWithoutRuntimeArtifactTestIT extends CamelLightCodeInsightFixtureTestCaseIT {
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        ServiceContainerUtil.registerServiceInstance(getProject(), CamelService.class, new CamelService(getProject()) {
+            @Override
+            public ArtifactCoordinates getProjectCamelCoreCoordinates() {
+                return ArtifactCoordinates.of("org.apache.camel", "camel-core-engine", "3.18.1");
+            }
+        });
+    }
+
+    /**
+     * Ensure that the Karaf Catalog can be retrieved.
+     */
+    public void testGetKarafCatalog() {
+        testGetCatalog(CamelCatalogProvider.KARAF);
+    }
+
+    /**
+     * Ensure that the Spring Boot Catalog can be retrieved.
+     */
+    public void testGetSpringBootCatalog() {
+        testGetCatalog(CamelCatalogProvider.SPRING_BOOT);
+    }
+
+    private void testGetCatalog(CamelCatalogProvider provider) {
+        CamelCatalog catalog = provider.get(getProject());
+        assertNotNull(catalog.getRuntimeProvider());
+        assertSame(catalog, catalog.getRuntimeProvider().getCamelCatalog());
+        assertInstanceOf(catalog, DefaultCamelCatalog.class);
+    }
+
+    /**
+     * Ensure that the Karaf runtime provider can be loaded.
+     */
+    public void testKarafRuntimeProvider() {
+        testRuntimeProvider(CamelCatalogProvider.KARAF);
+    }
+
+    /**
+     * Ensure that the SpringBoot runtime provider can be loaded.
+     */
+    public void testSpringBootRuntimeProvider() {
+        testRuntimeProvider(CamelCatalogProvider.SPRING_BOOT);
+    }
+
+    private void testRuntimeProvider(CamelCatalogProvider provider) {
+        getProject().getService(CamelCatalogService.class).get().setVersionManager(new CamelMavenVersionManager());
+        assertTrue(provider.loadRuntimeProviderVersion(getProject()));
+    }
+}
