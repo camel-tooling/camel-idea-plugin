2556b77659e0823ac0354746191af407a2c5d07d
shubh411 <100116292+shubh411@users.noreply.github.com>
2023-04-01 11:10:36 -0400

    Extract Class

--- camel-idea-plugin/src/main/java/com/github/cameltooling/idea/completion/header/CamelHeaderEndpointSource.java
+++ camel-idea-plugin/src/main/java/com/github/cameltooling/idea/completion/header/CamelHeaderEndpointSource.java
@@ -1,19 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.github.cameltooling.idea.completion.header;
 
 import java.util.Collection;
@@ -22,71 +6,96 @@ import java.util.stream.Stream;
 
 import com.github.cameltooling.idea.util.CamelIdeaUtils;
 import com.github.cameltooling.idea.util.StringUtils;
+import com.intellij.openapi.module.Module;
 import com.intellij.openapi.module.ModuleUtilCore;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.psi.PsiElement;
 import org.jetbrains.annotations.NotNull;
 
-/**
- * Defines all the supported source of endpoints.
- */
 public enum CamelHeaderEndpointSource {
-    /**
-     * Represents the source that provides all the producer endpoints.
-     */
     PRODUCER_ONLY {
         @Override
         Collection<CamelHeaderEndpoint> getEndpoints(@NotNull PsiElement element) {
-            final CamelIdeaUtils utils = CamelIdeaUtils.getService();
-            return utils.findEndpointUsages(
-                ModuleUtilCore.findModuleForPsiElement(element), e -> e.indexOf(':') != -1
-            )
-                .stream()
-                .filter(utils::isProducerEndpoint)
-                .map(PsiElement::getText)
-                .map(StringUtil::unquoteString)
-                .map(StringUtils::asComponentName)
-                .map(CamelHeaderEndpoint::producerOnly)
-                .collect(Collectors.toSet());
+            return new ProducerEndpointFinder(element).findEndpoints();
         }
     },
-    /**
-     * Represents the source that provides all the consumer endpoints.
-     */
+
     CONSUMER_ONLY {
         @Override
         Collection<CamelHeaderEndpoint> getEndpoints(@NotNull PsiElement element) {
-            final CamelIdeaUtils utils = CamelIdeaUtils.getService();
-            return utils.findEndpointDeclarations(
-                ModuleUtilCore.findModuleForPsiElement(element), e -> e.indexOf(':') != -1
-            )
-                .stream()
-                .filter(utils::isConsumerEndpoint)
-                .map(PsiElement::getText)
-                .map(StringUtil::unquoteString)
-                .map(StringUtils::asComponentName)
-                .map(CamelHeaderEndpoint::consumerOnly)
-                .collect(Collectors.toSet());
+            return new ConsumerEndpointFinder(element).findEndpoints();
         }
     },
-    /**
-     * Represents the source that provides all the existing endpoints.
-     */
+
     ALL {
         @Override
         Collection<CamelHeaderEndpoint> getEndpoints(@NotNull PsiElement element) {
-            return Stream.concat(
-                CONSUMER_ONLY.getEndpoints(element).stream(), PRODUCER_ONLY.getEndpoints(element).stream()
-            )
-                .map(CamelHeaderEndpoint::getComponentName)
-                .map(CamelHeaderEndpoint::both)
-                .collect(Collectors.toSet());
+            final Collection<CamelHeaderEndpoint> consumerEndpoints = CONSUMER_ONLY.getEndpoints(element);
+            final Collection<CamelHeaderEndpoint> producerEndpoints = PRODUCER_ONLY.getEndpoints(element);
+
+            return Stream.concat(consumerEndpoints.stream(), producerEndpoints.stream())
+                    .map(CamelHeaderEndpoint::getComponentName)
+                    .map(CamelHeaderEndpoint::both)
+                    .collect(Collectors.toSet());
         }
     };
 
-    /**
-     * @param element the element from which we retrieve the module for which we want the endpoints.
-     * @return a collection of all matching endpoints in the module corresponding to the given element.
-     */
     abstract Collection<CamelHeaderEndpoint> getEndpoints(@NotNull PsiElement element);
+
+    private static abstract class EndpointFinder {
+        final PsiElement element;
+
+        EndpointFinder(PsiElement element) {
+            this.element = element;
+        }
+
+        Collection<CamelHeaderEndpoint> findEndpoints() {
+            final CamelIdeaUtils utils = CamelIdeaUtils.getService();
+            final Module module = ModuleUtilCore.findModuleForPsiElement(element);
+
+            return utils.findEndpointUsages(module, e -> e.indexOf(':') != -1)
+                    .stream()
+                    .filter(this::matchesEndpointType)
+                    .map(PsiElement::getText)
+                    .map(StringUtil::unquoteString)
+                    .map(StringUtils::asComponentName)
+                    .map(this::createEndpoint)
+                    .collect(Collectors.toSet());
+        }
+
+        abstract boolean matchesEndpointType(PsiElement element);
+        abstract CamelHeaderEndpoint createEndpoint(String componentName);
+    }
+
+    private static class ProducerEndpointFinder extends EndpointFinder {
+        ProducerEndpointFinder(PsiElement element) {
+            super(element);
+        }
+
+        @Override
+        boolean matchesEndpointType(PsiElement element) {
+            return CamelIdeaUtils.getService().isProducerEndpoint(element);
+        }
+
+        @Override
+        CamelHeaderEndpoint createEndpoint(String componentName) {
+            return CamelHeaderEndpoint.producerOnly(componentName);
+        }
+    }
+
+    private static class ConsumerEndpointFinder extends EndpointFinder {
+        ConsumerEndpointFinder(PsiElement element) {
+            super(element);
+        }
+
+        @Override
+        boolean matchesEndpointType(PsiElement element) {
+            return CamelIdeaUtils.getService().isConsumerEndpoint(element);
+        }
+
+        @Override
+        CamelHeaderEndpoint createEndpoint(String componentName) {
+            return CamelHeaderEndpoint.consumerOnly(componentName);
+        }
+    }
 }
