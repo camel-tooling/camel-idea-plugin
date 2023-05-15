b3fe537b3b357d36c6aae03f597c48a5f64b4fc4
Nicolas Filotto <nfilotto@talend.com>
2023-05-15 19:29:34 +0200

    Reduce the build time

--- build-all.sh
+++ build-all.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
 for v in "2022.2.5" "2022.3.3" "2023.1.1"; do
-    echo "## Building with version $v..."
-    ./gradlew --no-daemon clean
-    ./gradlew --no-daemon --no-parallel -PideaVersion="$v" build
-
-    status=$?
-    if [[ $status -ne 0 ]]; then
-        echo "## Build for version $v failed. Exiting." >&2
-        exit 1
-    fi
+  ./build.sh $v
 done
